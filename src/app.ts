c8f5fd48897d690d159588a2f481f52e620406da
Ali0548 <abhai0548@gmail.com>
2023-11-24 10:29:18 +0500

    Express Js Setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-import fs from 'fs';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors';
+import helmet from 'helmet';
+import { json } from 'body-parser';
+const app = express();
+app.use(json({ limit: '50mb' }));
+app.use(express.json());
+const port = process.env.PORT;
+app.use(cors({ origin: true, credentials: true }));
+app.use(helmet());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
