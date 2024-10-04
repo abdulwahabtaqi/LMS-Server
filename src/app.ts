75d2fe58a766297f74b333f84fbc620392066f05
imranf620 <imranf620@gmail.com>
2024-10-05 00:18:58 +0500

    fixed cors issue

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,19 @@ import helmet from 'helmet';
 import { json } from 'body-parser';
 const app = express();
 import { v2 as cloudinary } from 'cloudinary';
+
+app.use(cors({
+    origin: "https://lms-client-hazel.vercel.app",
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 app.use(json({ limit: '50mb' }));
 app.use(express.json({
     limit: "20mb"
 }));
 const port = process.env.PORT || 4000;
-app.use(cors({
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
+
 app.use(helmet());
 import routes from './api/routes';
 
