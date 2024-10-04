4a55e72206b4baf9af6ca75540268f5a859d0227
imranf620 <imranf620@gmail.com>
2024-10-04 22:32:51 +0500

    fixed cors issue

--- src/app.ts
+++ src/app.ts
@@ -6,24 +6,17 @@ import helmet from 'helmet';
 import { json } from 'body-parser';
 const app = express();
 import { v2 as cloudinary } from 'cloudinary';
-import routes from './api/routes';
-
-
-app.use(cors({
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
+app.use(json({ limit: '50mb' }));
 app.use(express.json({
     limit: "20mb"
 }));
-app.use(json({ limit: '50mb' }));
-app.use(helmet());
-
 const port = process.env.PORT;
-// app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
+app.use(helmet());
+import routes from './api/routes';
+
+
+
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
