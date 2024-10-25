7261ac85cdfe2706c4fed206b322c611bf608170
imranf620 <imranf620@gmail.com>
2024-10-25 22:01:13 +0500

    added assignment functionality

--- src/api/controllers/assign/assignment.ts
+++ src/api/controllers/assign/assignment.ts
@@ -93,3 +93,74 @@ export const getConnAssign = async (req: Request, res: Response) => {
         return ApiResponse(false, "Error fetching assignments", error.message || error, 500, res);
     }
 };
+
+
+export const getTeacherAssignments = async (req: Request, res: Response) => {
+    try {
+        const { teacherId } = req.params;
+
+        const assignments = await prisma.assignmentQuestion.findMany({
+            where: {
+                userId: teacherId,
+            },
+            orderBy: {
+                createdAt: 'desc'
+            },
+            include: {
+                user: true,
+                questions: {
+                    include: {
+                        answers: true,
+                    },
+                },
+                school: true,
+                grade: true,
+                subject: true,
+                topic: true,
+                subTopic: true,
+            },
+        });
+
+        assignments.forEach(assignment => {
+            assignment.questions.forEach(question => {
+                const uniqueAnswers = Array.from(
+                    new Map(question.answers.map(answer => [answer.answer, answer])).values()
+                );
+                question.answers = uniqueAnswers;
+            });
+        });
+
+        return ApiResponse(true, "Teacher's assignments fetched successfully", assignments, 200, res);
+    } catch (error) {
+        return ApiResponse(false, "Error fetching teacher's assignments", error.message || error, 500, res);
+    }
+};
+
+
+export const deleteAssignment = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { teacherId } = req.params;
+
+        const assignment = await prisma.assignmentQuestion.findUnique({
+            where: { id: id },
+            select: { userId: true }
+        });
+
+        if (!assignment) {
+            return ApiResponse(false, "Assignment not found", null, 404, res);
+        }
+
+        if (assignment.userId !== teacherId) {
+            return ApiResponse(false, "You are not authorized to delete this assignment", null, 403, res);
+        }
+
+        const deletedAssignment = await prisma.assignmentQuestion.delete({
+            where: { id: id },
+        });
+
+        return ApiResponse(true, "Assignment deleted successfully", deletedAssignment, 200, res);
+    } catch (error) {
+        return ApiResponse(false, "Error deleting assignment", error.message || error, 500, res);
+    }
+};
--- src/api/routes/assign.ts
+++ src/api/routes/assign.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 const router = Router();
-import { createAssignment, getConnAssign } from '../controllers/assign/assignment';
+import { createAssignment, deleteAssignment, getConnAssign, getTeacherAssignments } from '../controllers/assign/assignment';
 
 router.post('/create', createAssignment);
 router.get('/all/:id', getConnAssign);
+router.get('/my/:id', getTeacherAssignments);
+router.delete('/delete/:id/teacher/:teacherId', deleteAssignment);
 
 
 export default router;
